60791ab60f8fdc533880bb99405a515d00539845
Prayag Sangode <67312119+prayag-sangode@users.noreply.github.com>
2022-11-28 11:40:35 +0530

    Create wif-gitlab.sh

---
+++ wif-gitlab.sh
@@ -0,0 +1,18 @@
+#WorkLoad Identity Federation - GitLab
+
+ISSUER=https://gitlab.com
+POOL=gitlab-wif
+PROVIDER=gitlab
+PROJECT=
+SA=
+gcloud iam workload-identity-pools create $POOL \
+  --location=global
+gcloud iam workload-identity-pools providers create-oidc $PROVIDER \
+  --workload-identity-pool=$POOL \
+  --issuer-uri=$ISSUER \
+  --location=global \
+  --attribute-mapping=google.subject=assertion.sub
+gcloud iam service-accounts add-iam-policy-binding $SA \
+  --role=roles/iam.workloadIdentityUser \
+  --member="principalSet://iam.googleapis.com/projects/$PROJECT/locations/global/workloadIdentityPools/$POOL/*"
+echo audience must be https://iam.googleapis.com/projects/$PROJECT/locations/global/workloadIdentityPools/$POOL/providers/$PROVIDER
