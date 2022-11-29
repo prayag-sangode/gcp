5c334762f25c44ce0ccf6d57b3c22d503ea5d3ba
Prayag Sangode <67312119+prayag-sangode@users.noreply.github.com>
2022-11-29 10:34:56 +0530

    Update wif-gitlab.sh

--- wif-gitlab.sh
+++ wif-gitlab.sh
@@ -44,21 +44,4 @@ gcloud iam service-accounts add-iam-policy-binding "$SERVICE_ACCOUNT_EMAIL" --pr
 
 gcloud iam workload-identity-pools providers describe $PROVIDER --project=$GCP_PROJECT_ID --location="global" --workload-identity-pool=$POOL --format="value(name)"
 
-#WorkLoad Identity Federation - GitLab
-
-#ISSUER=https://gitlab.com
-#POOL=gitlab-wif
-#PROVIDER=gitlab
-#PROJECT=
-#SA=
-#gcloud iam workload-identity-pools create $POOL \
-  --location=global
-#gcloud iam workload-identity-pools providers create-oidc $PROVIDER \
-  --workload-identity-pool=$POOL \
-  --issuer-uri=$ISSUER \
-  --location=global \
-  --attribute-mapping=google.subject=assertion.sub
-#gcloud iam service-accounts add-iam-policy-binding $SA \
-  --role=roles/iam.workloadIdentityUser \
-  --member="principalSet://iam.googleapis.com/projects/$PROJECT/locations/global/workloadIdentityPools/$POOL/*"
-#echo audience must be https://iam.googleapis.com/projects/$PROJECT/locations/global/workloadIdentityPools/$POOL/providers/$PROVIDER
+export  GOOGLE_APPLICATION_CREDENTIALS=.gcp_temp_cred.json
