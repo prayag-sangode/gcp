bff9d7f44f64db7692ae15dd19c8134a11d41b03
Prayag Sangode <67312119+prayag-sangode@users.noreply.github.com>
2023-01-15 14:54:10 +0530

    Update wif-jenkins.sh

--- wif-jenkins.sh
+++ wif-jenkins.sh
@@ -1,10 +1,8 @@
-#WorkLoad Identity Federation - Jenkins
-
-ISSUER=https://jenkins/oidc
-POOL=jenkins-wif
-PROVIDER=jenkins
-PROJECT=
-SA=
+ISSUER=http://35.229.246.122:8080/
+PROJECT=mypoc-374706
+POOL=jenkins-oidc-pool
+PROVIDER=static
+SA="GCP-SA@mypoc-374706.iam.gserviceaccount.com"
 gcloud iam workload-identity-pools create $POOL \
   --location=global
 gcloud iam workload-identity-pools providers create-oidc $PROVIDER \
@@ -16,4 +14,3 @@ gcloud iam service-accounts add-iam-policy-binding $SA \
   --role=roles/iam.workloadIdentityUser \
   --member="principalSet://iam.googleapis.com/projects/$PROJECT/locations/global/workloadIdentityPools/$POOL/*"
 echo audience must be https://iam.googleapis.com/projects/$PROJECT/locations/global/workloadIdentityPools/$POOL/providers/$PROVIDER
-
